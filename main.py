47f31acdd88b9cedb4193d55a4ada5fa79a21e0e
Daniel Abraham <daniel_abraham1@berkeley.edu>
2021-07-13 23:00:46 -0700

    Added a motion extraction function to find the motion signal from an arbitrary pilot tone

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import matplotlib.pyplot as plt
 from utils import sig_utils, MR_utils
 from PIL import Image
 
+# Uncertanty in TR
+tr_uncert = 0.01
+
 # Load MR image
 im = np.array(Image.open('images/brain.png'))
 # ksp = np.load('kspace/500_30sec.npz')['ksp'][:,:,0,3].T
-ksp = np.random.normal(0, 1, im.shape) + 1j * np.random.normal(0, 1, im.shape)
+ksp = np.random.normal(0, 2, im.shape) + 1j * np.random.normal(0, 2, im.shape)
 
 # Initialize MR object with the parameters below
-mr = MR_utils(tr=34.5e-3, bwpp=250e3/256, fc=127.8e6)
+mr = MR_utils(tr=34.876e-3, bwpp=250e3/256, fc=127.8e6)
+
+# Motion modulation signal
+def mod(t):
+	return 1000 + 100 * np.sin(2 * np.pi * t / (50 * mr.TR))
 
 # Load an image and add a pilot tone to it. 
-# mr.load_image(im)
-mr.load_kspace(ksp)
-mr.add_PT(127.8e6 + 80e3)
+mr.load_image(im)
+#mr.load_kspace(ksp)
+a, b = mr.add_PT(127.8e6 - 100e3, tr_uncert=tr_uncert, modulation=mod)
+motion = mr.motion_extract()
+plt.plot(np.abs(motion))
 mr.MRshow(drng=1e-6, log=True)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from PIL.Image import new
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
 class sig_utils:
 	Fs = 1e6
@@ -48,6 +49,9 @@ class sig_utils:
 			plt.plot(f, sig_fft)
 		plt.show()
 	
+	def gen_bp_filter(f_low, f_high, ntaps, fs):
+		return signal.firwin(ntaps, [f_low, f_high], pass_zero=False, fs=fs)
+
 	def plot_sig(t, sig):
 		plt.plot(t, sig)
 		plt.show()
@@ -82,14 +86,14 @@ class MR_utils:
 		self.fs = self.ksp.shape[1] * self.BWPP
 	
 	# Display image and kspace
-	def MRshow(self, drng=1e-6, log=True):
+	def MRshow(self, drng=1e-6, log=True, title=''):
 		# Check that we have images
 		if self.img is None or self.ksp is None:
 			print('Load image first')
 			return
 
 		# Generate subplots
-		fig, (ax1, ax2, ax3) = plt.subplots(ncols=3)
+		fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(10, 6))
 		
 		# Display kspace (left)
 		ax1.set_title('K-Space')
@@ -107,7 +111,8 @@ class MR_utils:
 		# Display image (right)
 		ax3.set_title('Acquired Image')
 		ax3.imshow(np.abs(self.img), cmap='gray')
-
+		
+		fig.suptitle(title)
 		plt.show()
 
 	# 2DFT and inverse 
@@ -117,24 +122,54 @@ class MR_utils:
 		return np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(F)))
 	
 	# Adds a pure pilot tone to our kspace data
-	def add_PT(self, freq, modulation=None):
+	def add_PT(self, freq, tr_uncert=0, modulation=None):
 		# Demodulate by center frequency
 		new_freq = freq - self.fc
 		
-		# Use signal util to generate a pure tone
-		sig_utils.Fs = self.fs
+		# Number of phase encodes and readouts
 		n_pe, n_ro = self.ksp.shape
-		_, sig = sig_utils.gen_tone(new_freq, 0, self.TR * n_pe, complex=True)
-		sig *= 10
 
 		# Add pilot tone to kspace data 
-		sample_num = 0
 		for pe in range(n_pe):
-			# Add phase accumulated by waiting until the next readout
-			sample_num += int(self.fs * (self.TR - (1 / self.BWPP)))
+			phase_accrued = (pe * self.TR)
+			if tr_uncert != 0:
+				phase_accrued *= 1 + (((2 * np.random.rand()) - 1) * tr_uncert)
 			for ro in range(n_ro):
-				self.ksp[pe, ro] += sig[sample_num]
-				sample_num += 1
+				t = phase_accrued + ro / self.fs
+				if modulation is None:
+					self.ksp[pe, ro] += 2700 * np.exp(2j*np.pi*new_freq*t)
+				else: 
+					self.ksp[pe, ro] += modulation(t) * np.exp(2j*np.pi*new_freq*t)
 		
 		# Recalculate image
 		self.img = MR_utils.ifft2c(self.ksp)
+
+		# Calculate pilot tone location
+		val = new_freq / self.fs
+		beta = np.round(val) - val
+		b = np.round(n_ro/2 + beta * n_ro)
+
+		val = new_freq * self.TR
+		alpha = np.round(val) - val
+		a = np.round(n_pe/2 + alpha * n_pe)
+
+		return a, b
+
+	# Extracts Motion signal from Pilot Tone
+	def motion_extract(self):
+		# First we take the K-Space FFT along the readout direction
+		fft_readout = np.fft.fftshift(np.fft.fft(self.ksp, axis=1), axes=1)
+
+		# Now we search for a strong vertical line corelation
+		n_pe, n_ro = self.ksp.shape
+		amax = np.argmax(np.sum(np.abs(fft_readout), axis=0))
+
+		# # Now that we have the pilot tone frequency, we want to filter our kspace 
+		# margin = 1e3
+		# fpt = np.abs((amax/n_ro - 1/2) * self.fs)
+		# filt = sig_utils.gen_bp_filter(fpt - margin, fpt + margin, 129, self.fs)
+		# new_ksp = np.apply_along_axis(lambda m: np.convolve(m, filt, mode='same'), axis=1, arr=self.ksp)
+		# fft_readout = np.fft.fftshift(np.fft.fft(new_ksp, axis=1), axes=1)
+
+		# Finally exract the motion signal now that we have the index
+		return fft_readout[:, amax]
